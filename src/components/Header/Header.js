1e9aeea2c2ef01efd45546ae1fac2b26ab78852b
Talha <mdtalha2008@gmail.com>
2022-09-29 02:24:11 +0600

    header complete

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import logo from '../../images/icons8-football-64.png';
+import './Header.css';
 const Header = () => {
     return (
-        <div>
-            <h1>This is header</h1>
-        </div>
+       <nav className='header'>
+            <img src={logo} alt=''></img>
+            <h4>Football Clubs</h4>
+       </nav>
     );
 };
 
