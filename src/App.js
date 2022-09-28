15fe7b3816010a06da45e543b64102cfe59c9a86
Talha <mdtalha2008@gmail.com>
2022-09-29 03:32:27 +0600

    products json add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header/Header';
+import Player from './components/Player/Player';
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <Player></Player>
     </div>
   );
 }
---
+++ src/components/Player/Player.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Player.css'
+
+const Player = () => {
+    return (
+        <div className='player-container'>
+            <div>
+                <h4>Select Best Players</h4>
+            </div>
+            <div>
+                <h4>Goal Time</h4>
+            </div>
+        </div>
+    );
+};
+
+export default Player;
