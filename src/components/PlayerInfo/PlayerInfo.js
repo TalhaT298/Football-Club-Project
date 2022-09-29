b5e3dcc738cb2e488fa7351858e338d2b009cb8b
Talha <mdtalha2008@gmail.com>
2022-09-29 17:42:42 +0600

    assingment

--- src/components/PlayerInfo/PlayerInfo.js
+++ src/components/PlayerInfo/PlayerInfo.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const PlayerInfo = () => {
+    const {name,img,}=this.props.player;
     return (
         <div>
-            
+            <h2>This is Produ</h2>
         </div>
     );
 };
