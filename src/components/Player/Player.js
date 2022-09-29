ba008096ba64f66e1181a659934f06cbe6583fe8
Talha <mdtalha2008@gmail.com>
2022-09-29 11:08:37 +0600

    fake data improve

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PlayerInfo from '../PlayerInfo/PlayerInfo';
 import './Player.css'
 
 const Player = () => {
+    const [players,setPlayers]=useState([]);
+
+    useEffect(()=>{
+        fetch('products.json')
+        .then(res=>res.json())
+        .then(data=>console.log(data))
+    },[])
     return (
         <div className='player-container'>
             <div>
-                <h4>Select Best Players</h4>
+                {
+                    products.map(player => <PlayerInfo></PlayerInfo>)
+                }
             </div>
             <div>
                 <h4>Goal Time</h4>
